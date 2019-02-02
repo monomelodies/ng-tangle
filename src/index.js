278bad31dea7b89e6866ad9f6aa46f8ef0d0c893
Marijn Ophorst <marijn@monomelodies.nl>
2019-02-02 19:01:25 +0100

    load template directive

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import defaultRouting from './defaultRouting';
 import Response from './Response';
 import watch from './watch';
 import submit from './submit';
+import template from './template';
 
 export default angular.module('ngTangle', ['ngRoute'])
     .config(html5mode)
     .config(defaultRouting)
     .service('tangleResponse', Response)
     .run(watch)
+    .directive('tangleTemplate', template)
     .directive('tangleSubmit', submit)
     .name;
 
